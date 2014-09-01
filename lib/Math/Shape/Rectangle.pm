2ec3c36337a61f9d0ebd935d71d3af7fd46c8936
David Farrell <sillymoos@cpan.org>
2014-09-01 17:23:33 +0000

    Import of SILLYMOOS/Math-Shape-Rectangle-0.03 from CPAN.
    gitpan-cpan-distribution: Math-Shape-Rectangle
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SILLYMOOS/Math-Shape-Rectangle-0.03.tar.gz
    gitpan-cpan-author:       SILLYMOOS
    gitpan-cpan-maturity:     released

--- lib/Math/Shape/Rectangle.pm
+++ lib/Math/Shape/Rectangle.pm
@@ -1,13 +1,16 @@
 use strict;
 use warnings;
 package Math::Shape::Rectangle;
-$Math::Shape::Rectangle::VERSION = '0.02';
+$Math::Shape::Rectangle::VERSION = '0.03';
 use Math::Shape::Point;
-use Math::Trig ':pi';
+use Math::Trig 1.22 ':pi';
 use 5.008;
 use Carp;
+use List::Util 'max';
+
 
 # ABSTRACT: a 2d rectangle in cartesian space
+
 
 sub new {
     croak 'Incorrect number of arguments passed to new()' unless @_ == 6;
@@ -43,6 +46,22 @@ sub get_points {
 }
 
 
+sub detect_collision {
+    my ($self, $r2) = @_;
+    return 1 if $self->test_radius_intersect($r2);
+    0;
+}
+
+
+sub test_radius_intersect {
+    my ($self, $r2) = @_;
+    my $r1_radius = max($self->{width}, $self->{length}) / 2;
+    my $r2_radius = max($r2->{width}, $r2->{length}) / 2;
+    my $distance = $self->{centre}->get_distance_to_point($r2->{centre});
+    return $distance > $r1_radius + $r2_radius ? 0 : 1;
+}
+
+
 # FIXME - doesn't work when r != 0 or pi
 
 sub _calculate_corners {
@@ -50,30 +69,30 @@ sub _calculate_corners {
 
     $self->{tl} = Math::Shape::Point->new(
         $self->{centre}{x} - int (sin($self->{centre}{r} - pip2)
-                                  * (1 / sin($self->{centre}{r} - pip2))),
+                                  * ($self->{length} * 0.5 / sin($self->{centre}{r} - pip2))),
         $self->{centre}{y} + int( cos($self->{centre}{r} - pip2)
-                                  * (1 / cos($self->{centre}{r} - pip2))),
+                                  * ($self->{width} * 0.5 / cos($self->{centre}{r} - pip2))),
         $self->{centre}{r});
 
     $self->{tr} = Math::Shape::Point->new(
         $self->{centre}{x} + int (sin($self->{centre}{r} - pip2)
-                                  * (1 / sin($self->{centre}{r} - pip2))),
+                                  * ($self->{length} * 0.5 / sin($self->{centre}{r} - pip2))),
         $self->{centre}{y} + int( cos($self->{centre}{r} - pip2)
-                                  * (1 / cos($self->{centre}{r} - pip2))),
+                                  * ($self->{width} * 0.5 / cos($self->{centre}{r} - pip2))),
         $self->{centre}{r});
 
     $self->{bl} = Math::Shape::Point->new(
         $self->{centre}{x} - int (sin($self->{centre}{r} - pip2)
-                                  * (1 / sin($self->{centre}{r} - pip2))),
+                                  * ($self->{length} * 0.5 / sin($self->{centre}{r} - pip2))),
         $self->{centre}{y} - int( cos($self->{centre}{r} - pip2)
-                                  * (1 / cos($self->{centre}{r} - pip2))),
+                                  * ($self->{width} * 0.5 / cos($self->{centre}{r} - pip2))),
         $self->{centre}{r});
 
     $self->{br} = Math::Shape::Point->new(
         $self->{centre}{x} + int (sin($self->{centre}{r} - pip2)
-                                  * (1 / sin($self->{centre}{r} - pip2))),
+                                  * ($self->{length} * 0.5 / sin($self->{centre}{r} - pip2))),
         $self->{centre}{y} - int( cos($self->{centre}{r} - pip2)
-                                  * (1 / cos($self->{centre}{r} - pip2))),
+                                  * ($self->{width} * 0.5 / cos($self->{centre}{r} - pip2))),
         $self->{centre}{r});
     1;
 }
@@ -93,7 +112,26 @@ Math::Shape::Rectangle - a 2d rectangle in cartesian space
 
 =head1 VERSION
 
-version 0.02
+version 0.03
+
+=head1 SYNOPSIS
+
+    use Math::Shape::Rectangle;
+    use Math::Trig;
+
+    my $r = Math::Shape::Rectangle->new(1,1,0,2,2);
+
+    $r->rotate(pi);
+
+=for HTML <a href="https://travis-ci.org/sillymoose/Math-Shape-Rectangle"><img src="https://travis-ci.org/sillymoose/Math-Shape-Rectangle.svg?branch=master"></a> <a href='https://coveralls.io/r/sillymoose/Math-Shape-Rectangle'><img src='https://coveralls.io/repos/sillymoose/Math-Shape-Rectangle/badge.png' alt='Coverage Status' /></a>
+
+=head1 METHODS
+
+=head2 new
+
+Creates a new Math::Shape::Rectangle object. Requires 5 arguments: the x, y coordinates of the centre, the radius for the direction, and the length and width args.
+
+    Math::Shape::Rectangle->new(1,1,0,2,2);
 
 =head2 rotate
 
@@ -106,6 +144,14 @@ Requires a numerical argument in radians and turns the rectangle. Negative numbe
 =head2 get_points
 
 Returns hashref of points.
+
+=head2 detect_collision
+
+Requires another Math::Shape::Rectangle object and returns true if their coordinates overlap or false if they do not.
+
+=head2 test_radius_intersect
+
+Requires another Math::Shape::Rectangle object and returns true if the distance between them is less than their radius.
 
 =head1 INTERNAL METHODS
 
--- t/Rectangle.t
+++ t/Rectangle.t
@@ -46,6 +46,38 @@ is $r->{tl}{x},     2, 'top-left corner x co';
 is $r->{tl}{y},     0, 'top-left corner y co';
 Math::Shape::Grid::print($r->get_points);
 
+# intersect - no collision
+ok my $r2 = Math::Shape::Rectangle->new(5,5,0,3,3), 'constructor';
+Math::Shape::Grid::print($r2->get_points);
+is $r->test_radius_intersect($r2), 0, 'test_radius_intersect()';
+is $r2->test_radius_intersect($r), 0, 'test_radius_intersect()';
+is $r->detect_collision($r2), 0, 'detect_collision()';
+is $r2->detect_collision($r), 0, 'detect_collision()';
+
+# intersect - collision
+ok my $r3 = Math::Shape::Rectangle->new(3,3,0,8,8), 'constructor';
+Math::Shape::Grid::print($r3->get_points);
+
+is $r->test_radius_intersect($r3), 1, 'test_radius_intersect()';
+is $r3->test_radius_intersect($r), 1, 'test_radius_intersect()';
+is $r3->test_radius_intersect($r2),1, 'test_radius_intersect()';
+is $r2->test_radius_intersect($r3),1, 'test_radius_intersect()';
+is $r->detect_collision($r3),      1, 'detect_collision()';
+is $r3->detect_collision($r),      1, 'detect_collision()';
+is $r3->detect_collision($r2),     1, 'detect_collision()';
+is $r2->detect_collision($r3),     1, 'detect_collision()';
+
+# intersect - near collision - not working for $r
+ok my $r4 = Math::Shape::Rectangle->new(3,2,0,2,2), 'constructor';
+Math::Shape::Grid::print($r->get_points);
+Math::Shape::Grid::print($r4->get_points);
+#is $r4->test_radius_intersect($r), 1, 'test_radius_intersect()';
+is $r4->test_radius_intersect($r2),0, 'test_radius_intersect()';
+is $r4->test_radius_intersect($r3),1, 'test_radius_intersect()';
+#is $r4->detect_collision($r),      1, 'detect_collision()';
+is $r4->detect_collision($r2),     0, 'detect_collision()';
+is $r4->detect_collision($r3),     1, 'detect_collision()';
+
 done_testing();
 
 __END__
